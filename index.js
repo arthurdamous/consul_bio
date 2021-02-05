077f1d451765a3d77b2579b75e3083643ed7e853
arthur <arthur.damous@gmail.com>
2021-02-04 22:39:56 -0300

    Implementado Cross-env

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 const express = require('express');
+const morgan = require('morgan');
 const path = require('path');
 
 const app = express();
 
 app.use(express.static('static'));
 
+if(process.env.NODE_ENV === 'development' ){
+    app.use(morgan('dev'));
+}
+
 app.get( '/', (req, res) => {
     res.sendFile(path.join(__dirname, 'static', 'index.html'))
 })
 
-const PORT = 3000;
 
-const server = app.listen(5000, 'localhost', () => {
-    console.log('Servidor está em funcionamento')
-})
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on ${PORT}`));
 
 
