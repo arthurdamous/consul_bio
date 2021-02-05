7e1a29c0b859fcce31bde2a51566c557567b2e80
arthur <arthur.damous@gmail.com>
2021-02-04 22:51:14 -0300

    Alteraçao

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ app.get( '/', (req, res) => {
     res.sendFile(path.join(__dirname, 'static', 'index.html'))
 })
 
-
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on ${PORT}`));
 
